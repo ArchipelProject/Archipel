f7f4794922db667f9990a086f36cf5a186427c1f
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2010-07-05 18:13:53 +0200

    * bug resolution in Health module

--- ArchipelServer/modules/hypervisor_health/__init__.py
+++ ArchipelServer/modules/hypervisor_health/__init__.py
@@ -14,14 +14,14 @@ def __module_init__health_module(self):
     db_file                 = self.configuration.get("HEALTH", "health_database_path")
     collection_interval     = self.configuration.getint("HEALTH", "health_collection_interval")
     max_rows_before_purge   = self.configuration.getint("HEALTH", "max_rows_before_purge")
-    min_row_before_storing  = self.configuration.getint("HEALTH", "min_row_before_storing")
+    max_cached_rows  = self.configuration.getint("HEALTH", "max_cached_rows")
     #snmp_agent              = self.configuration.get("HEALTH", "health_snmp_agent")
     #snmp_community          = self.configuration.get("HEALTH", "health_snmp_community")
     #snmp_version            = self.configuration.getint("HEALTH", "health_snmp_version")
     #snmp_port               = self.configuration.getint("HEALTH", "health_snmp_port")
     #snmp_infos               = {"snmp_agent": snmp_agent, "snmp_community": snmp_community, "snmp_version": snmp_version, "snmp_port": snmp_port}
     
-    self.module_health = health.TNHypervisorHealth(db_file, collection_interval, max_rows_before_purge, min_row_before_storing) #, snmp_infos)
+    self.module_health = health.TNHypervisorHealth(db_file, collection_interval, max_rows_before_purge, max_cached_rows) #, snmp_infos)
 
 
 def __module_register_stanza__heatlh_module(self):
--- ArchipelServer/modules/hypervisor_health/archipelStatsCollector.py
+++ ArchipelServer/modules/hypervisor_health/archipelStatsCollector.py
@@ -23,21 +23,19 @@ import time
 from threading import Thread
 from utils import *
 
-#from pysnmp.entity.rfc3413.oneliner import cmdgen
-
 
 class TNThreadedHealthCollector(Thread):
     """
     this class collects hypervisor stats regularly
     """
-    def __init__(self, database_file, collection_interval, max_rows_before_purge, min_row_before_storing): #, snmp_agent, snmp_community, snmp_version, snmp_port):
+    def __init__(self, database_file, collection_interval, max_rows_before_purge, max_cached_rows):
         """
         the contructor of the class
         """
         self.database_file          = database_file
         self.collection_interval    = collection_interval
         self.max_rows_before_purge  = max_rows_before_purge
-        self.min_row_before_storing = min_row_before_storing
+        self.max_cached_rows        = max_cached_rows
         self.stats_CPU              = []
         self.stats_memory           = []
         self.stats_load             = []
@@ -57,28 +55,27 @@ class TNThreadedHealthCollector(Thread):
         
         self.recover_stored_stats()
         
-        
         Thread.__init__(self)
     
     
     def recover_stored_stats(self):
         log.info("recovering stored statistics")
-        self.cursor.execute("select * from cpu order by collection_date desc limit %d" % self.max_rows_before_purge)
+        self.cursor.execute("select * from cpu order by collection_date desc limit %d" % self.max_cached_rows)
         for values in self.cursor:
             date, idle = values
             self.stats_CPU.insert(0, {"date": date, "id": idle})
         
-        self.cursor.execute("select * from memory order by collection_date desc limit %d" % self.max_rows_before_purge)
+        self.cursor.execute("select * from memory order by collection_date desc limit %d" % self.max_cached_rows)
         for values in self.cursor:
             date, free, used, total, swapped = values
             self.stats_memory.insert(0, {"date": date, "free": free, "used" : used, "total": total, "swapped": swapped})
         
-        self.cursor.execute("select * from load order by collection_date desc limit %d" % self.max_rows_before_purge)
+        self.cursor.execute("select * from load order by collection_date desc limit %d" % self.max_cached_rows)
         for values in self.cursor:
             date, one, five, fifteen = values
             self.stats_load.insert(0, {"date": date, "one": one, "five": five, "fifteen": fifteen})
         
-        self.cursor.execute("select * from disk order by collection_date desc limit %d" % self.max_rows_before_purge)
+        self.cursor.execute("select * from disk order by collection_date desc limit %d" % self.max_cached_rows)
         for values in self.cursor:
             date, total, used, free, free_percentage = values
             self.stats_disks.insert(0, {"date": date, "total": total, "used": used, "free": free, "free_percentage": free_percentage})
@@ -91,11 +88,11 @@ class TNThreadedHealthCollector(Thread):
         @rtype: TNArchipelVirtualMachine
         @return: the L{TNArchipelVirtualMachine} instance
         """        
-        #log.debug("Retrieving last "+ str(limit) + " recorded stats data for sending")
+        log.debug("Retrieving last "+ str(limit) + " recorded stats data for sending")
         try:
             uptime = commands.getoutput("uptime").split("up ")[1].split(",")[0]
             uptime_stats = {"up" : uptime}
-            return {"cpu": self.stats_CPU[0:limit], "memory": self.stats_memory[0:limit], "disk": self.stats_disks[0:limit], "load": self.stats_load[0:limit], "uptime": uptime_stats, "uname": self.uname_stats}
+            return {"cpu": self.stats_CPU[-limit:], "memory": self.stats_memory[-limit:], "disk": self.stats_disks[-limit:], "load": self.stats_load[-limit:], "uptime": uptime_stats, "uname": self.uname_stats}
         except Exception as ex:
             log.error("stat recuperation fails. Exception %s" % str(ex))
             return None
@@ -136,6 +133,24 @@ class TNThreadedHealthCollector(Thread):
         return {"date": datetime.datetime.now(), "total" : total, "used": used, "free": free, "free_percentage": freePrct}
     
     
+    def getTimeList(self):
+        statFile = file("/proc/stat", "r")
+        timeList = statFile.readline().split(" ")[2:6]
+        statFile.close()
+        for i in range(len(timeList)):
+            timeList[i] = int(timeList[i])
+        return timeList
+    
+    
+    def deltaTime(self, interval):
+        x = self.getTimeList()
+        time.sleep(interval)
+        y = self.getTimeList()
+        for i in range(len(x))  :
+            y[i] -= x[i]
+        return y
+    
+    
     def run(self):
         """
         overiddes sur super class method. do the L{TNArchipelVirtualMachine} main loop
@@ -144,50 +159,39 @@ class TNThreadedHealthCollector(Thread):
         
         while(1):
             try:
-                self.stats_CPU.insert(0, self.get_cpu_stats())
-                self.stats_memory.insert(0, self.get_memory_stats())
-                self.stats_load.insert(0, self.get_load_stats())
-                self.stats_disks.insert(0, self.get_disk_stats())
+                self.stats_CPU.append(self.get_cpu_stats())
+                self.stats_memory.append(self.get_memory_stats())
+                self.stats_load.append(self.get_load_stats())
+                self.stats_disks.append(self.get_disk_stats())
                 
-                if len(self.stats_CPU) % self.min_row_before_storing == 0:
-                    self.database_thread_connection.executemany("insert into memory values(:date, :free, :used, :total, :swapped)", self.stats_memory)
-                    self.database_thread_connection.executemany("insert into cpu values(:date, :id)", self.stats_CPU)
-                    self.database_thread_connection.executemany("insert into load values(:date, :one , :five, :fifteen)", self.stats_load)
-                    self.database_thread_connection.executemany("insert into disk values(:date, :total, :used,:free, :free_percentage)", self.stats_disks)
+                if len(self.stats_CPU) >= self.max_cached_rows:
+                    middle = (self.max_cached_rows - 1) /  2
+                    
+                    self.database_thread_connection.executemany("insert into memory values(:date, :free, :used, :total, :swapped)", self.stats_memory[0:middle])
+                    self.database_thread_connection.executemany("insert into cpu values(:date, :id)", self.stats_CPU[0:middle])
+                    self.database_thread_connection.executemany("insert into load values(:date, :one , :five, :fifteen)", self.stats_load[0:middle])
+                    self.database_thread_connection.executemany("insert into disk values(:date, :total, :used,:free, :free_percentage)", self.stats_disks[0:middle])
                     log.info("stats saved in database file")
                     
-                    if len(self.stats_CPU) >= self.min_row_before_storing * 2:
-                        del self.stats_CPU[(-self.min_row_before_storing - 1)]
-                        del self.stats_memory[(-self.min_row_before_storing - 1)]
-                        del self.stats_load[(-self.min_row_before_storing - 1)]
-                        del self.stats_disks[(-self.min_row_before_storing - 1)]
-                        log.info("cached stats have been purged from memory")
+                    del self.stats_CPU[0:middle]
+                    del self.stats_memory[0:middle]
+                    del self.stats_load[0:middle]
+                    del self.stats_disks[0:middle]
+                    log.info("cached stats have been purged from memory")
                         
-                    if int(self.database_thread_connection.execute("select count(*) from memory").fetchone()[0]) >= self.max_rows_before_purge:
+                    if int(self.database_thread_connection.execute("select count(*) from memory").fetchone()[0]) >= self.max_rows_before_purge * 2:
                         self.database_thread_connection.execute("delete from cpu where collection_date=(select collection_date from cpu order by collection_date asc limit "+ str(self.max_rows_before_purge) +")")
                         self.database_thread_connection.execute("delete from memory where collection_date=(select collection_date from memory order by collection_date asc limit "+ str(self.max_rows_before_purge) +")")
                         self.database_thread_connection.execute("delete from load where collection_date=(select collection_date from load order by collection_date asc limit "+ str(self.max_rows_before_purge) +")")
                         self.database_thread_connection.execute("delete from disk where collection_date=(select collection_date from disk order by collection_date asc limit "+ str(self.max_rows_before_purge) +")")
                         log.debug("old stored stats have been purged from memory")
-                    
+                        
                     self.database_thread_connection.commit()
-                
+                    
                 time.sleep(self.collection_interval)
             except Exception as ex:
                 log.error("stat collection fails. Exception %s" % str(ex))
-            
-    def getTimeList(self):
-        statFile = file("/proc/stat", "r")
-        timeList = statFile.readline().split(" ")[2:6]
-        statFile.close()
-        for i in range(len(timeList)):
-            timeList[i] = int(timeList[i])
-        return timeList
+    
 
-    def deltaTime(self, interval)  :
-        x = self.getTimeList()
-        time.sleep(interval)
-        y = self.getTimeList()
-        for i in range(len(x))  :
-            y[i] -= x[i]
-        return y
+
+
