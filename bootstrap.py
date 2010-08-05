3c79aa2e580ec838e2ce55e45b29fe847ee22db4
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2010-08-05 17:43:20 +0200

    * bootstrap use capp gen

--- bootstrap.py
+++ bootstrap.py
@@ -34,7 +34,7 @@ def build_archipel():
 
 def apply_cappuccino_frameworks():
     print "* Adding Cappuccino framework"
-    os.system("/usr/local/narwhal/bin/capp -f ./ArchipelClient")
+    os.system("/usr/local/narwhal/bin/capp gen -f ./ArchipelClient")
     print "* Cappuccino added"
 
 if __name__ == "__main__":
