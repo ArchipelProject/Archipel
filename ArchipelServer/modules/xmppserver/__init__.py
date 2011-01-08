4e975a61c4f323d9b4e021650a635954b6f30030
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-01-08 16:06:17 +0100

    check if the given xmpp server control bin is valid or aborting module loading

--- ArchipelServer/modules/xmppserver/__init__.py
+++ ArchipelServer/modules/xmppserver/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import xmppserver
 import archipel
-
+import os
 
 ARCHIPEL_NS_XMPPSERVER_GROUPS  = "archipel:xmppserver:groups"
 ARCHIPEL_NS_XMPPSERVER_USERS   = "archipel:xmppserver:users"
@@ -9,10 +9,14 @@ ARCHIPEL_NS_XMPPSERVER_USERS   = "archipel:xmppserver:users"
 # this method will be call at loading
 def __module_init__xmppserver(self):
     exec_path = self.configuration.get("XMPPSERVER", "exec_path")
+    if not os.path.exists(exec_path):
+        self.log.warning("unable to find %s command. aborting loading of module XMPPServer" % exec_path)
+        return
     self.module_xmppserver = xmppserver.TNXMPPServerController(self, exec_path=exec_path)
 
 # this method will be called at registration of handlers for XMPP
 def __module_register_stanza__xmppserver(self):
+    if not self.module_xmppserver: return
     self.xmppclient.RegisterHandler('iq', self.module_xmppserver.process_groups_iq, ns=ARCHIPEL_NS_XMPPSERVER_GROUPS)
     self.xmppclient.RegisterHandler('iq', self.module_xmppserver.process_users_iq, ns=ARCHIPEL_NS_XMPPSERVER_USERS)
 
