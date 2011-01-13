f113eb5ada2965b9ca979a62794623dcb91bc250
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-01-13 17:12:44 +0100

    small markup update

--- ci.py
+++ ci.py
@@ -39,59 +39,59 @@ def pullSubrepo():
     global BUILD_MESSAGEBOARD
     global BUILD_GROWLCAPPUCCINO
     
-    os.system("echo Checking if we need to build Cappuccino...")
+    os.system("echo \* Checking if we need to build Cappuccino...")
     ret, out = commands.getstatusoutput("cd ./Cappuccino && git pull origin master")
     if ret: sys.exit(-421)
     if "Already up-to-date." in out: BUILD_CAPPUCCINO=False
-    os.system("echo build Cappuccino: %s" % (str(BUILD_CAPPUCCINO)))
+    os.system("echo \* build Cappuccino: %s" % (str(BUILD_CAPPUCCINO)))
     
-    os.system("echo Checking if we need to build LPKit...")
+    os.system("echo \* Checking if we need to build LPKit...")
     ret, out = commands.getstatusoutput("cd ./LPKit && git pull origin integ")
     if ret: sys.exit(-422)
     if "Already up-to-date." in out: BUILD_LPKIT=False
-    os.system("echo build LPKit: %s" % (str(BUILD_LPKIT)))
+    os.system("echo \* build LPKit: %s" % (str(BUILD_LPKIT)))
     
-    os.system("echo Checking if we need to build StropheCappuccino...")
+    os.system("echo \* Checking if we need to build StropheCappuccino...")
     ret, out = commands.getstatusoutput("cd ./StropheCappuccino && git pull origin master")
     if ret: sys.exit(-423)
     if "Already up-to-date." in out: BUILD_STROPHECAPPUCCINO=False
-    os.system("echo build StropheCappuccino: %s" % (str(BUILD_STROPHECAPPUCCINO)))
+    os.system("echo \* build StropheCappuccino: %s" % (str(BUILD_STROPHECAPPUCCINO)))
     
-    os.system("echo Checking if we need to build TNKit...")
+    os.system("echo \* Checking if we need to build TNKit...")
     ret, out = commands.getstatusoutput("cd ./TNKit && git pull origin master")
     if ret: sys.exit(-424)
     if "Already up-to-date." in out: BUILD_TNKIT=False
-    os.system("echo build TNKit: %s" % (str(BUILD_TNKIT)))
+    os.system("echo \* build TNKit: %s" % (str(BUILD_TNKIT)))
     
-    os.system("echo Checking if we need to build VNCCappuccino...")
+    os.system("echo \* Checking if we need to build VNCCappuccino...")
     ret, out = commands.getstatusoutput("cd ./VNCCappuccino && git pull origin master")
     if ret: sys.exit(-425)
     if "Already up-to-date." in out: BUILD_VNCCAPPUCCINO=False
-    os.system("echo build VNCCappuccino: %s" % (str(BUILD_VNCCAPPUCCINO)))
+    os.system("echo \* build VNCCappuccino: %s" % (str(BUILD_VNCCAPPUCCINO)))
     
-    os.system("echo Checking if we need to build GrowlCappuccino...")
+    os.system("echo \* Checking if we need to build GrowlCappuccino...")
     ret, out = commands.getstatusoutput("cd ./GrowlCappuccino && git pull origin master")
     if ret: sys.exit(-426)
     if "Already up-to-date." in out: BUILD_GROWLCAPPUCCINO=False
-    os.system("echo build GrowlCappuccino: %s" % (str(BUILD_GROWLCAPPUCCINO)))
+    os.system("echo \* build GrowlCappuccino: %s" % (str(BUILD_GROWLCAPPUCCINO)))
     
-    os.system("echo Checking if we need to build iTunesTabView...")
+    os.system("echo \* Checking if we need to build iTunesTabView...")
     ret, out = commands.getstatusoutput("cd ./iTunesTabView && git pull origin master")
     if ret: sys.exit(-427)
     if "Already up-to-date." in out: BUILD_ITUNESTABVIEW=False
-    os.system("echo build iTunesTabView: %s" % (str(BUILD_ITUNESTABVIEW)))
+    os.system("echo \* build iTunesTabView: %s" % (str(BUILD_ITUNESTABVIEW)))
     
-    os.system("echo Checking if we need to build MessageBoard...")
+    os.system("echo \* Checking if we need to build MessageBoard...")
     ret, out = commands.getstatusoutput("cd ./MessageBoard && git pull origin master")
     if ret: sys.exit(-428)
     if "Already up-to-date." in out: BUILD_MESSAGEBOARD=False    
-    os.system("echo build MessageBoard: %s" % (str(BUILD_MESSAGEBOARD)))
+    os.system("echo \* build MessageBoard: %s" % (str(BUILD_MESSAGEBOARD)))
     
-    # os.system("echo Checking if we need to build ArchipelClient...")
+    # os.system("echo \* Checking if we need to build ArchipelClient...")
     # ret, out = commands.getstatusoutput("git pull origin master")
     # if ret: sys.exit(-428)
     # if "Already up-to-date." in out: BUILD_ARCHIPELCLIENT=False    
-    # os.system("echo build ArchipelClient: %s" % (str(BUILD_ARCHIPELCLIENT)))
+    # os.system("echo \* build ArchipelClient: %s" % (str(BUILD_ARCHIPELCLIENT)))
     
 
 def buildCappuccino():
@@ -103,47 +103,48 @@ def buildCappuccino():
 
 def buildGrowlCappuccino():
     if os.system("cd ./GrowlCappuccino && jake release &&jake debug"):
-        os.system("echo unable to build GrowlCappuccino")
+        os.system("echo \* unable to build GrowlCappuccino")
         sys.exit(-2)
 
 
 def buildiTunesTabView():
     if os.system("cd ./iTunesTabView && jake release && jake debug"):
-        os.system("echo unable to build iTunesTabView")
+        os.system("echo \* unable to build iTunesTabView")
         sys.exit(-3)
 
 
 def buildLPKit():
     if os.system("cd ./LPKit && export CONFIGURATION=Debug && jake -f myJakeFile build && export CONFIGURATION=Release && jake -f myJakeFile build"):
-        os.system("echo unable to build LPKit")
+        os.system("echo \* unable to build LPKit")
         sys.exit(-4)
 
 
 def buildMessageBoard():
     if os.system("cd ./MessageBoard && jake release && jake debug"):
-        os.system("echo unable to build MessageBoard")
+        os.system("echo \* unable to build MessageBoard")
         sys.exit(-5)
 
 
 def buildStropheCappuccino():
     if os.system("cd ./StropheCappuccino && jake release && jake debug"):
-        os.system("echo unable to build StropheCappuccino")
+        os.system("echo \* unable to build StropheCappuccino")
         sys.exit(-6)
 
 
 def buildTNKit():
     if os.system("cd ./TNKit && jake release && jake debug"):
-        os.system("echo unable to build TNKit")
+        os.system("echo \* unable to build TNKit")
         sys.exit(-7)
 
 
 def buildVNCCappuccino():
     if os.system("cd ./VNCCappuccino && jake release && jake debug"):
-        os.system("echo unable to build VNCCappuccino")
+        os.system("echo \* unable to build VNCCappuccino")
         sys.exit(-8)
 
 
 def buildArchipel(export_dir, build):
+    os.system("echo \* Starting to build Archipel")
     token   = datetime.datetime.now().strftime("%y%m%d-%H-%M")
     folder  = "%s/%s" % (export_dir, token)
     os.system("mkdir -p %s" % folder)
@@ -175,5 +176,5 @@ if __name__ == "__main__":
     
     buildArchipel(DEPLOY_PATH, BUILD_ARCHIPELCLIENT)
     
-    os.system("echo BUILD SUCESSFULL.")
+    os.system("echo \* BUILD SUCESSFULL.")
     sys.exit(0)
