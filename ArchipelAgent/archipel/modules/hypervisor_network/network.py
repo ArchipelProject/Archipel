b246235b84979e2ab5a3e2819c4ff11d0c1cfa49
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-02-14 14:28:46 +0100

    add vocabulary to control hypervisor's networks

--- ArchipelAgent/archipel/modules/hypervisor_network/network.py
+++ ArchipelAgent/archipel/modules/hypervisor_network/network.py
@@ -41,11 +41,6 @@ class TNHypervisorNetworks:
         @param entity the module entity
         """
         self.entity = entity
-        self.libvirt_connection = libvirt.open(self.entity.configuration.get("GLOBAL", "libvirt_uri"))
-        if self.libvirt_connection == None:
-            self.entity.log.error("unable to connect libvirt")
-            sys.exit(0) 
-        self.entity.log.info("connected to  libvirt")
         
         # permissions
         if self.entity.__class__.__name__ == "TNArchipelVirtualMachine":
@@ -59,11 +54,110 @@ class TNHypervisorNetworks:
             self.entity.permission_center.create_permission("network_get",      "Authorizes user to get all networks informations", False)
             self.entity.permission_center.create_permission("network_getnames", "Authorizes user to get the existing network names", False)
             self.entity.permission_center.create_permission("network_bridges",  "Authorizes user to get existing bridges", False)
+            
+            registrar_items = [
+                                {   "commands" : ["list networks"], 
+                                    "parameters": {}, 
+                                    "method": self.message_get,
+                                    "permissions": ["network_get"],
+                                    "description": "List all networks" },
+                                {   "commands" : ["create network", "start network"], 
+                                    "parameters": [{"name": "identifier", "description": "The identifer of the network, UUID or name"}], 
+                                    "method": self.message_create,
+                                    "permissions": ["network_create"],
+                                    "description": "Start the given network" },
+                                {   "commands" : ["destroy network", "stop network"], 
+                                    "parameters": [{"name": "identifier", "description": "The identifer of the network, UUID or name"}], 
+                                    "method": self.message_destroy,
+                                    "permissions": ["network_destroy"],
+                                    "description": "Stop the given network" }
+                                ]
+
+            self.entity.add_message_registrar_items(registrar_items)
     
     
+    
+    ### libvirt controls
+    
+    def get(self, active=True, inactive=True):
+        """
+        get the list of the networks
+        
+        @type active bool
+        @param active: if True, will return active network
+        @type inactive bool
+        @param inactive: if True, will return not active network
+        
+        @rtype: dict
+        @return a list containing networtks
+        """
+        ret = {}
+        if active:      ret["active"]   = self.entity.libvirt_connection.listNetworks()
+        if inactive:    ret["inactive"] = self.entity.libvirt_connection.listDefinedNetworks()
+        return ret
+    
+    
+    def create(self, identifier):
+        """
+        create (start) the network with given identifier
+        
+        @type identifier string
+        @param identifier: the identifer of the network to create. It can be its name or UUID
+        """
+        try:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByUUIDString(identifier)
+        except:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByName(identifier)
+        libvirt_network.create()
+        self.entity.log.info("virtual network %s created" % identifier)
+        self.entity.push_change("network", "created", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+    
+    
+    def destroy(self, identifier):
+        """
+        destroy (stop) the network with given identifier
+        
+        @type identifier string
+        @param identifier: the identifer of the network to destroy. It can be its name or UUID
+        """
+        try:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByUUIDString(identifier)
+        except:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByName(identifier)
+        libvirt_network.destroy()
+        self.entity.log.info("virtual network %s destroyed" % identifier)
+        self.entity.push_change("network", "destroyed", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+    
+    
+    def define(self, definition):
+        """
+        define the network
+        
+        @type definition string
+        @param definition: the XML definition to use
+        """
+        self.entity.libvirt_connection.networkDefineXML(str(definition))
+        self.entity.log.info("virtual network XML is defined")
+        self.entity.push_change("network", "defined", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+    
+    
+    def undefine(self, identifier):
+        """
+        undefine the network with given identifier
+        
+        @type identifier string
+        @param identifier: the identifer of the network to destroy. It can be its name or UUID
+        """
+        try:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByUUIDString(identifier)
+        except:
+            libvirt_network = self.entity.libvirt_connection.networkLookupByName(identifier)
+        libvirt_network.undefine()
+        self.entity.log.info("virtual network %s is undefined" % identifier)
+        self.entity.push_change("network", "undefined", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+        
     
     ### XMPP Processing
-    
     
     def process_iq_for_hypervisor(self, conn, iq):
         """
@@ -125,6 +219,7 @@ class TNHypervisorNetworks:
             raise xmpp.protocol.NodeProcessed
     
     
+    
     def iq_define(self, iq):
         """
         Define a virtual network in the libvirt according to the XML data
@@ -137,12 +232,9 @@ class TNHypervisorNetworks:
         @return: a ready to send IQ containing the result of the action
         """
         try:
-            network_node = iq.getTag("query").getTag("archipel").getTag("network")
-            
-            reply = iq.buildReply("result")
-            self.libvirt_connection.networkDefineXML(str(network_node))
-            self.entity.log.info("virtual network XML is defined")
-            self.entity.push_change("network", "defined", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+            reply           = iq.buildReply("result")
+            network_node    = iq.getTag("query").getTag("archipel").getTag("network")            
+            self.define(network_node)
         except libvirt.libvirtError as ex:
             reply = build_error_iq(self, ex, iq, ex.get_error_code(), ns=ARCHIPEL_NS_LIBVIRT_GENERIC_ERROR)
         except Exception as ex:
@@ -160,19 +252,16 @@ class TNHypervisorNetworks:
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
-        reply = None
         try:
-            network_uuid = iq.getTag("query").getTag("archipel").getAttr("uuid")
-            libvirt_network = self.libvirt_connection.networkLookupByUUIDString(network_uuid)
-            libvirt_network.undefine()
             reply = iq.buildReply("result")
-            self.entity.log.info("virtual network XML is undefined")
-            self.entity.push_change("network", "undefined", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
+            network_uuid = iq.getTag("query").getTag("archipel").getAttr("uuid")
+            self.undefine(network_uuid)
         except libvirt.libvirtError as ex:
             reply = build_error_iq(self, ex, iq, ex.get_error_code(), ns=ARCHIPEL_NS_LIBVIRT_GENERIC_ERROR)
         except Exception as ex:
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_NETWORKS_UNDEFINE)
         return reply
+    
     
     
     def iq_create(self, iq):
@@ -185,20 +274,31 @@ class TNHypervisorNetworks:
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
-        reply = None
         try:
             network_uuid = iq.getTag("query").getTag("archipel").getAttr("uuid")
-            libvirt_network = self.libvirt_connection.networkLookupByUUIDString(network_uuid)
-            libvirt_network.create()
+            self.create(network_uuid)
             reply = iq.buildReply("result")
-            self.entity.log.info("virtual network created")
-            self.entity.push_change("network", "created", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
             self.entity.shout("network", "Network %s has been started by %s." % (network_uuid, iq.getFrom()), excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
         except libvirt.libvirtError as ex:
             reply = build_error_iq(self, ex, iq, ex.get_error_code(), ns=ARCHIPEL_NS_LIBVIRT_GENERIC_ERROR)
         except Exception as ex:
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_NETWORKS_CREATE)
         return reply
+    
+    
+    def message_create(self, msg):
+        """
+        handle the creation request message
+        """
+        try:
+            tokens = msg.getBody().split()
+            if not len(tokens) == 3: return "I'm sorry, you use a wrong format. You can type 'help' to get help"
+            identifier = tokens[-1:][0]
+            self.create(identifier);
+            return "Starting network %s" % identifier
+        except Exception as ex:
+            return build_error_message(self, ex)
+    
     
     
     def iq_destroy(self, iq):
@@ -211,20 +311,31 @@ class TNHypervisorNetworks:
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
-        reply = None
         try:
             network_uuid = iq.getTag("query").getTag("archipel").getAttr("uuid")
-            libvirt_network = self.libvirt_connection.networkLookupByUUIDString(network_uuid)
-            libvirt_network.destroy()
+            self.destroy(network_uuid)
             reply = iq.buildReply("result")
-            self.entity.log.info("virtual network destroyed")
-            self.entity.push_change("network", "destroyed", excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
             self.entity.shout("network", "Network %s has been shutdwned by %s." % (network_uuid, iq.getFrom()), excludedgroups=[ARCHIPEL_XMPP_GROUP_VM, ARCHIPEL_XMPP_GROUP_HYPERVISOR])
         except libvirt.libvirtError as ex:
             reply = build_error_iq(self, ex, iq, ex.get_error_code(), ns=ARCHIPEL_NS_LIBVIRT_GENERIC_ERROR)
         except Exception as ex:
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_NETWORKS_DESTROY)
         return reply
+    
+    
+    def message_destroy(self, msg):
+        """
+        handle the destroying request message
+        """
+        try:
+            tokens = msg.getBody().split()
+            if not len(tokens) == 3: return "I'm sorry, you use a wrong format. You can type 'help' to get help"
+            identifier = tokens[-1:][0]
+            self.destroy(identifier);
+            return "Destroying network %s" % identifier
+        except Exception as ex:
+            return build_error_message(self, ex)
+    
     
     
     def iq_get(self, iq):
@@ -238,33 +349,49 @@ class TNHypervisorNetworks:
         @return: a ready to send IQ containing the result of the action
         """
         try:
-            reply = iq.buildReply("result")
-            active_networks_nodes = [] 
-            not_active_networks_nodes = [] #xmpp.Node(tag="unactivedNetworks")
-        
-            actives_networks_names = self.libvirt_connection.listNetworks()
-            not_active_networks_names = self.libvirt_connection.listDefinedNetworks()
-        
-        
-            for network_name in actives_networks_names:
-                network = self.libvirt_connection.networkLookupByName(network_name)
+            reply                   = iq.buildReply("result")
+            active_networks_nodes   = [] 
+            inactive_networks_nodes = []
+            networks                = self.get()
+            
+            for network_name in networks["active"]:
+                network = self.entity.libvirt_connection.networkLookupByName(network_name)
                 desc = network.XMLDesc(0)
                 n = xmpp.simplexml.NodeBuilder(data=desc).getDom()
                 active_networks_nodes.append(n)
-        
-            for network_name in not_active_networks_names:
-                network = self.libvirt_connection.networkLookupByName(network_name)
+            
+            for network_name in networks["inactive"]:
+                network = self.entity.libvirt_connection.networkLookupByName(network_name)
                 desc = network.XMLDesc(0)
                 n = xmpp.simplexml.NodeBuilder(data=desc).getDom()
-                not_active_networks_nodes.append(n)
-            active_networks_root_node = xmpp.Node(tag="activedNetworks", payload=active_networks_nodes)
-            not_active_networks_root_node = xmpp.Node(tag="unactivedNetworks", payload=not_active_networks_nodes)
-            reply.setQueryPayload([active_networks_root_node, not_active_networks_root_node])
+                inactive_networks_nodes.append(n)
+            
+            active_networks_root_node   = xmpp.Node(tag="activedNetworks", payload=active_networks_nodes)
+            inactive_networks_root_node = xmpp.Node(tag="unactivedNetworks", payload=inactive_networks_nodes)
+            
+            reply.setQueryPayload([active_networks_root_node, inactive_networks_root_node])
         except libvirt.libvirtError as ex:
             reply = build_error_iq(self, ex, iq, ex.get_error_code(), ns=ARCHIPEL_NS_LIBVIRT_GENERIC_ERROR)
         except Exception as ex:
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_NETWORKS_GET)
         return reply
+    
+    
+    def message_get(self, msg):
+        """
+        create the message response to list network
+        """
+        try:
+            networks = self.get()
+            ret = "Sure. Here are my current active networks:\n"
+            for net in networks["active"]: ret += "    - %s\n" % net
+            ret += "\n and my unactive network:\n"
+            for net in networks["inactive"]: ret += "    - %s\n" % net
+            return ret
+        except Exception as ex:
+            return build_error_message(self, ex)
+        
+    
     
     
     def iq_get_names(self, iq):
@@ -280,7 +407,7 @@ class TNHypervisorNetworks:
         try:
             reply = iq.buildReply("result")
             active_networks_nodes = [] 
-            actives_networks_names = self.libvirt_connection.listNetworks()
+            actives_networks_names = self.get(inactive=False)["active"]
             for network_name in actives_networks_names:
                 network = xmpp.Node(tag="network", attrs={"name": network_name})
                 active_networks_nodes.append(network)
