c640bad43017a0d63ced9462c9007f3880548873
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-02-21 22:56:15 +0100

    update version of eggs

--- ArchipelAgent/archipel-agent/setup.py
+++ ArchipelAgent/archipel-agent/setup.py
@@ -18,7 +18,7 @@
 import os, shutil
 from setuptools import setup, find_packages
 
-VERSION             = '1.0.14'
+VERSION             = '1.0.15'
 DESCRIPTION="""\
 ** Archipel Agent **
 
@@ -69,16 +69,16 @@ setup(name='archipel-agent',
       install_requires=[
         "xmpppy>=0.5.0rc1",
         "sqlobject>=0.14.1",
-        "archipel-agent-action-scheduler>=1.0.12beta",
-        "archipel-agent-hypervisor-geolocalization>=1.0.12beta",
-        "archipel-agent-hypervisor-health>=1.0.13beta",
-        "archipel-agent-hypervisor-network>=1.0.12beta",
-        "archipel-agent-iphone-notification>=1.0.12beta",
-        "archipel-agent-virtualmachine-oomkiller>=1.0.12beta",
-        "archipel-agent-virtualmachine-snapshoting>=1.0.12beta",
-        "archipel-agent-virtualmachine-storage>=1.0.12beta",
-        "archipel-agent-vmcasting>=1.0.12beta",
-        "archipel-agent-xmppserver>=1.0.13beta"
+        "archipel-agent-action-scheduler>=1.0.13beta",
+        "archipel-agent-hypervisor-geolocalization>=1.0.13beta",
+        "archipel-agent-hypervisor-health>=1.0.14beta",
+        "archipel-agent-hypervisor-network>=1.0.13beta",
+        "archipel-agent-iphone-notification>=1.0.13beta",
+        "archipel-agent-virtualmachine-oomkiller>=1.0.13beta",
+        "archipel-agent-virtualmachine-snapshoting>=1.0.13beta",
+        "archipel-agent-virtualmachine-storage>=1.0.13beta",
+        "archipel-agent-vmcasting>=1.0.13beta",
+        "archipel-agent-xmppserver>=1.0.14beta"
       ],
       entry_points="""
         # -*- Entry points: -*-
