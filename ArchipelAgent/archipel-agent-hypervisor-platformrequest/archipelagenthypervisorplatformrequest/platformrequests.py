627b65d7dbb9dddd95d78cb81154852751fdfa53
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-03-01 11:18:30 +0100

    add support for external score computing unit and use two pubsub queue for input requests and output answers

--- ArchipelAgent/archipel-agent-hypervisor-platformrequest/archipelagenthypervisorplatformrequest/platformrequests.py
+++ ArchipelAgent/archipel-agent-hypervisor-platformrequest/archipelagenthypervisorplatformrequest/platformrequests.py
@@ -19,10 +19,12 @@ import xmpp
 import os
 import archipel
 from libvirt import *
+from pkg_resources import iter_entry_points
 
 from archipelcore.utils import *
 from archipelcore.pubsub import *
 from archipelcore.archipelPlugin import TNArchipelPlugin
+from scorecomputing import TNBasicPlatformScoreComputing
 
 
 class TNPlatformRequests (TNArchipelPlugin):
@@ -35,7 +37,12 @@ class TNPlatformRequests (TNArchipelPlugin):
         """
         TNArchipelPlugin.__init__(self, configuration=configuration, entity=entity, entry_point_group=entry_point_group)
         
-        self.pubsub_vmrequestnode = None
+        self.pubsub_request_in_node     = None
+        self.pubsub_request_out_node    = None
+        self.computing_unit             = None
+        
+        # get eventual computing unit plugin
+        self.load_computing_unit()
         
         # register to the node vmrequest
         self.entity.register_hook("HOOK_ARCHIPELENTITY_XMPP_AUTHENTICATED", self.manage_platform_vm_request)
@@ -65,28 +72,52 @@ class TNPlatformRequests (TNArchipelPlugin):
     
     
     
-    ### Utilities
+    ### Plugin loading
     
+    def load_computing_unit(self):
+        """
+        loads the external computing unit
+        """
+        for factory_method in iter_entry_points(group="archipel.plugin.platform.computingunit", name="factory"):
+            method              = factory_method.load()
+            plugin_content      = method()
+            self.computing_unit = plugin_content["plugin"]
+            self.entity.log.info("PLATFORMREQ: loading computing unit %s" % plugin_content["info"]["common-name"])
+            break
+        if not self.computing_unit: 
+            self.computing_unit = TNBasicPlatformScoreComputing()
+            self.entity.log.info("PLATFORMREQ: using default computing unit")
+        
+    
+    ### Performs platform actions
     
     def perform_virtual_machine_creation(self, request):
-        return (True, xmpp.Node("anwser"))
+        return (self.computing_unit.score(), xmpp.Node("anwser"))
     
+    
+    ### Pubsub management
     
     def manage_platform_vm_request(self, entity, args):
         """
-        register to pubsub event node /archipel/platform/vmrequests
+        register to pubsub event node /archipel/platform/requests/in
+        and /archipel/platform/requests/out
         """
-        nodeVMRequestsName = "/archipel/platform/vmrequests"
-        self.entity.log.info("getting the pubsub node %s" % nodeVMRequestsName)
-        self.pubsub_vmrequestnode = TNPubSubNode(self.entity.xmppclient, self.entity.pubsubserver, nodeVMRequestsName)
-        self.pubsub_vmrequestnode.recover()
-        self.entity.log.info("node %s recovered" % nodeVMRequestsName)
-        self.pubsub_vmrequestnode.subscribe(self.entity.jid.getStripped(), self._handle_vmrequest_event)
-        self.entity.log.info("entity %s is now subscribed to events from node %s" % (self.entity.jid, nodeVMRequestsName))
+        nodeVMRequestsInName = "/archipel/platform/requests/in"
+        self.entity.log.info("PLATFORMREQ: getting the pubsub node %s" % nodeVMRequestsInName)
+        self.pubsub_request_in_node = TNPubSubNode(self.entity.xmppclient, self.entity.pubsubserver, nodeVMRequestsInName)
+        self.pubsub_request_in_node.recover()
+        self.entity.log.info("PLATFORMREQ: node %s recovered" % nodeVMRequestsInName)
+        self.pubsub_request_in_node.subscribe(self.entity.jid.getStripped(), self._handle_request_event)
+        self.entity.log.info("PLATFORMREQ: entity %s is now subscribed to events from node %s" % (self.entity.jid, nodeVMRequestsInName))
+        
+        nodeVMRequestsOutName = "/archipel/platform/requests/out"
+        self.entity.log.info("PLATFORMREQ: getting the pubsub node %s" % nodeVMRequestsOutName)
+        self.pubsub_request_out_node = TNPubSubNode(self.entity.xmppclient, self.entity.pubsubserver, nodeVMRequestsOutName)
+        self.pubsub_request_out_node.recover()
+        self.entity.log.info("PLATFORMREQ: node %s recovered" % nodeVMRequestsOutName)
         
     
-    
-    def _handle_vmrequest_event(self, event):
+    def _handle_request_event(self, event):
         """
         triggered when a platform wide virtual machine request is received
         """
@@ -96,18 +127,15 @@ class TNPlatformRequests (TNArchipelPlugin):
             item_publisher = xmpp.JID(item.getAttr("publisher"))
             if not item_publisher.getStripped() == self.entity.jid.getStripped():
                 try:
-                    self.entity.log.info("received a platform-wide virtual machine request from %s (NOT IMPLEMENTED YET)" % item_publisher)
+                    self.entity.log.info("PLATFORMREQ: received a platform-wide virtual machine request from %s (NOT IMPLEMENTED YET)" % item_publisher)
                     request_uuid = item.getTag("archipel").getAttr("uuid")
-                    answer, content = self.perform_virtual_machine_creation(item)
-                    if answer:
-                        answer_node = xmpp.Node("archipel", attrs={"uuid": request_uuid})
+                    score, content = self.perform_virtual_machine_creation(item)
+                    if score:
+                        answer_node = xmpp.Node("archipel", attrs={"uuid": request_uuid, "score": score})
                         answer_node.addChild(node=content)
-                        self.pubsub_vmrequestnode.add_item(answer_node)
+                        self.pubsub_request_out_node.add_item(answer_node)
                 except Exception as ex:
-                    self.entity.log.error("seems that request is not valid (%s) %s" % (str(ex), str(item)))
-                
-        
-        ## RTO
+                    self.entity.log.error("PLATFORMREQ: seems that request is not valid (%s) %s" % (str(ex), str(item)))
     
     
 
---
+++ ArchipelAgent/archipel-agent-hypervisor-platformrequest/archipelagenthypervisorplatformrequest/scorecomputing.py
@@ -0,0 +1,77 @@
+# 
+# scorecomputing.py
+# 
+# Copyright (C) 2010 Antoine Mercadal <antoine.mercadal@inframonde.eu>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class TNBasicPlatformScoreComputing (object):
+    """
+    this class is a basic score computing. If you want to provide
+    your own score computing unit, you can subclass this and implement
+    your own computing system
+    """
+    
+    
+    ### Initialization
+    
+    def __init__(self):
+        """
+        put custom initialization here
+        """
+        pass
+    
+    
+    
+    ## Plugin
+    
+    @staticmethod
+    def plugin_info():
+        """
+        return inforations about the plugin
+        """
+        plugin_friendly_name           = "Platform Request Base Score Computing Unit"
+        plugin_identifier              = "basecomputingunit"
+        plugin_configuration_section   = None
+        plugin_configuration_tokens    = []
+        
+        return {    "common-name"               : plugin_friendly_name, 
+                    "identifier"                : plugin_identifier,
+                    "configuration-section"     : plugin_configuration_section,
+                    "configuration-tokens"      : plugin_configuration_tokens }
+    
+    
+    
+    ## Score computing
+    
+    def score(self, action=None):
+        """
+        perform the score. The highest the score, the highest the 
+        you got chance to perform the action. If you want to decline
+        the performing of the action, return 0.0 or None. the max score
+        you can return is 1.0 (so basically see it as a percentage)
+        
+        @type action: string
+        @param action: the name of the action if you want to use it to compute the score (optionnal)
+        
+        @rtype: float
+        @return: the score
+        """
+        
+        ## awesome computing goes here
+        
+        import random
+        return random.random() # yeah! that's a big computing!
+    
+
