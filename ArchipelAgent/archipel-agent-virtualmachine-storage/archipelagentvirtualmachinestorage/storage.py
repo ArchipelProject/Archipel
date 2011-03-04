eedb653351b32114f3ebd34f4f4da494a41cd7d4
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-03-04 11:42:13 +0100

    add an optionnal option in config to give the path of qemu-img + PEP8

--- ArchipelAgent/archipel-agent-virtualmachine-storage/archipelagentvirtualmachinestorage/storage.py
+++ ArchipelAgent/archipel-agent-virtualmachine-storage/archipelagentvirtualmachinestorage/storage.py
@@ -33,20 +33,30 @@ ARCHIPEL_ERROR_CODE_DRIVES_GETISO       = -3004
 ARCHIPEL_ERROR_CODE_DRIVES_CONVERT      = -3005
 ARCHIPEL_ERROR_CODE_DRIVES_RENAME       = -3006
 
+
 class TNStorageManagement (TNArchipelPlugin):
+    """
+    plugin that manages the storage volumes for virtual machines
+    """
 
     def __init__(self, configuration, entity, entry_point_group):
         """
         initialize the module
-        @type entity TNArchipelEntity
-        @param entity the module entity
+        @type configuration: Configuration object
+        @param configuration: the configuration
+        @type entity: L{TNArchipelEntity}
+        @param entity: the entity that owns the plugin
+        @type entry_point_group: string
+        @param entry_point_group: the group name of plugin entry_point
         """
         TNArchipelPlugin.__init__(self, configuration=configuration, entity=entity, entry_point_group=entry_point_group)
-
-        self.shared_isos_folder =  self.configuration.get("STORAGE", "iso_base_path") + "/"
-
-        if not os.path.exists( self.shared_isos_folder): os.makedirs(self.shared_isos_folder)
-
+        self.shared_isos_folder = self.configuration.get("STORAGE", "iso_base_path") + "/"
+        if self.configuration.has_option("STORAGE", "qemu_img_bin_path"):
+            self.qemu_img_bin = self.configuration.get("STORAGE", "qemu_img_bin_path")
+        else:
+            self.qemu_img_bin = "qemu-img"
+        if not os.path.exists(self.shared_isos_folder):
+            os.makedirs(self.shared_isos_folder)
         # permissions
         self.entity.permission_center.create_permission("drives_create", "Authorizes user to get create a drive", False)
         self.entity.permission_center.create_permission("drives_delete", "Authorizes user to delete a drive", False)
@@ -64,7 +74,6 @@ class TNStorageManagement (TNArchipelPlugin):
         necessary to register module for listening to stanza
         """
         self.entity.xmppclient.RegisterHandler('iq', self.process_iq, ns=ARCHIPEL_NS_VM_DISK)
-
 
     @staticmethod
     def plugin_info():
@@ -84,11 +93,9 @@ class TNStorageManagement (TNArchipelPlugin):
 
     ### XMPP Processing
 
-
     def process_iq(self, conn, iq):
         """
         Invoked when new ARCHIPEL_NS_VM_DISK IQ is received.
-
         it understands IQ of type:
         - create
         - delete
@@ -96,7 +103,6 @@ class TNStorageManagement (TNArchipelPlugin):
         - getiso
         - convert
         - rename
-
         @type conn: xmpp.Dispatcher
         @param conn: ths instance of the current connection that send the message
         @type iq: xmpp.Protocol.Iq
@@ -105,27 +111,29 @@ class TNStorageManagement (TNArchipelPlugin):
         reply = None
         action = self.entity.check_acp(conn, iq)
         self.entity.check_perm(conn, iq, action, -1, prefix="drives_")
-
-        if self.entity.is_migrating and (not action in ("get", "getiso")): reply = build_error_iq(self, "virtual machine is migrating. Can't perform any drives operation", iq, ARCHIPEL_ERROR_CODE_VM_MIGRATING)
-        elif action == "create":    reply = self.iq_create(iq)
-        elif action == "delete":    reply = self.iq_delete(iq)
-        elif action == "get":       reply = self.iq_get(iq)
-        elif action == "getiso":    reply = self.iq_getiso(iq)
-        elif action == "convert":   reply = self.iq_convert(iq)
-        elif action == "rename":    reply = self.iq_rename(iq)
-
+        if self.entity.is_migrating and (not action in ("get", "getiso")):
+            reply = build_error_iq(self, "virtual machine is migrating. Can't perform any drives operation", iq, ARCHIPEL_ERROR_CODE_VM_MIGRATING)
+        elif action == "create":
+            reply = self.iq_create(iq)
+        elif action == "delete":
+            reply = self.iq_delete(iq)
+        elif action == "get":
+            reply = self.iq_get(iq)
+        elif action == "getiso":
+            reply = self.iq_getiso(iq)
+        elif action == "convert":
+            reply = self.iq_convert(iq)
+        elif action == "rename":
+            reply = self.iq_rename(iq)
         if reply:
             conn.send(reply)
             raise xmpp.protocol.NodeProcessed
 
-
     def iq_create(self, iq):
         """
         Create a disk in given format
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
@@ -136,21 +144,15 @@ class TNStorageManagement (TNArchipelPlugin):
             disk_unit   = query_node.getTag("archipel").getAttr("unit")
             format      = query_node.getTag("archipel").getAttr("format")
             disk_path   = self.entity.folder + "/" + disk_name + "." + format
-
             if disk_unit == "M" and (int(disk_size) >= 1000000000):
                 raise Exception("too big",  "You may be able to do it manually, but I won't try")
             elif disk_unit == "G" and (int(disk_size) >= 10000):
                 raise Exception("too big", "You may be able to do this manually, but I won't try")
-
             if os.path.exists(disk_path):
                 raise Exception("The disk with name %s already exists." % disk_name)
-
-            #ret = os.system("qemu-img create -f " + format + " " + disk_path + " " + disk_size + disk_unit)
-            ret = subprocess.call(["qemu-img", "create", "-f", format, disk_path, "%s%s" % (disk_size, disk_unit)])
-
+            ret = subprocess.call([self.qemu_img_bin, "create", "-f", format, disk_path, "%s%s" % (disk_size, disk_unit)])
             if not ret == 0:
                 raise Exception("DriveError", "Unable to create drive. Error code is " + str(ret))
-
             reply = iq.buildReply("result")
             self.entity.log.info("disk created")
             self.entity.shout("disk", "I've just created a new hard drive named %s with size of %s%s." % (disk_name, disk_size, disk_unit))
@@ -159,14 +161,11 @@ class TNStorageManagement (TNArchipelPlugin):
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_CREATE)
         return reply
 
-
     def iq_convert(self, iq):
         """
         Convert a disk from a format to another
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
@@ -177,18 +176,13 @@ class TNStorageManagement (TNArchipelPlugin):
             path        = query_node.getTag("archipel").getAttr("path")
             format      = query_node.getTag("archipel").getAttr("format")
             disk_path   = path.replace(path.split(".")[-1], "") + format
-
             if os.path.exists(disk_path):
                 raise Exception("The disk with same name and extension already exists.")
-
             self.entity.change_presence(presence_show="dnd", presence_status="Converting a disk...")
-            #ret = os.system("qemu-img convert " + path + " -O " + format + " " + disk_path)
-            ret = subprocess.call(["qemu-img", "convert", path, "-O", format, disk_path])
+            ret = subprocess.call([self.qemu_img_bin, "convert", path, "-O", format, disk_path])
             if not ret == 0:
                 raise Exception("DriveError", "Unable to convert drive. Error code is " + str(ret))
-
             os.unlink(path)
-
             self.entity.change_presence(presence_show=old_show, presence_status=old_status)
             reply = iq.buildReply("result")
             self.entity.log.info("convertion of  created")
@@ -199,14 +193,11 @@ class TNStorageManagement (TNArchipelPlugin):
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_CONVERT)
         return reply
 
-
     def iq_rename(self, iq):
         """
         Rename a disk
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
@@ -214,15 +205,11 @@ class TNStorageManagement (TNArchipelPlugin):
             query_node = iq.getTag("query")
             path = query_node.getTag("archipel").getAttr("path")
             newname = query_node.getTag("archipel").getAttr("newname")
-
             extension = path.split(".")[-1]
-            newpath = os.path.join(self.entity.folder,  "%s.%s" % (newname, extension))
-
+            newpath = os.path.join(self.entity.folder, "%s.%s" % (newname, extension))
             if os.path.exists(newpath):
                 raise Exception("The disk with name %s already exists." % newname)
-
             os.rename(path, newpath)
-
             reply = iq.buildReply("result")
             self.entity.log.info("renamed hard drive %s into  %s" % (path, newname))
             self.entity.shout("disk", "I've just renamed hard drive %s into  %s." % (path, newname))
@@ -231,14 +218,11 @@ class TNStorageManagement (TNArchipelPlugin):
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_RENAME)
         return reply
 
-
     def iq_delete(self, iq):
         """
         delete a virtual hard drive
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
@@ -247,34 +231,26 @@ class TNStorageManagement (TNArchipelPlugin):
             disk_name           = query_node.getTag("archipel").getAttr("name")
             secure_disk_name    = disk_name.split("/")[-1]
             secure_disk_path    = self.entity.folder + "/" + secure_disk_name
-
-            old_status  = self.entity.xmppstatus
-            old_show    = self.entity.xmppstatusshow
+            old_status          = self.entity.xmppstatus
+            old_show            = self.entity.xmppstatusshow
             self.entity.change_presence(presence_show="dnd", presence_status="Deleting a drive...")
-
-            #os.system("rm -rf " + secure_disk_path)
             os.unlink(secure_disk_path)
-
             disk_nodes = []
             if self.entity.definition:
                 devices_node = self.entity.definition.getTag('devices')
                 disk_nodes = devices_node.getTags('disk', attrs={'type': 'file'})
-
-            if query_node.getTag("archipel").getAttr("undefine") ==  "yes":
+            if query_node.getTag("archipel").getAttr("undefine") == "yes":
                 have_undefined_at_least_on_disk = False
                 for disk_node in disk_nodes:
                     path = disk_node.getTag('source').getAttr('file')
                     if path == secure_disk_path:
                         devices_node.delChild(disk_node)
                         have_undefined_at_least_on_disk = True
-
                 if have_undefined_at_least_on_disk:
                     xml = str(self.entity.definition).replace('xmlns="http://www.gajim.org/xmlns/undeclared" ', '')
                     self.entity.libvirt_connection.defineXML(xml)
                     self.entity.push_change("virtualmachine:definition", "defined")
-
             self.entity.change_presence(presence_show=old_show, presence_status=old_status)
-
             reply = iq.buildReply("result")
             self.entity.log.info("disk %s deleted" % secure_disk_path)
             self.entity.push_change("disk", "deleted")
@@ -283,21 +259,17 @@ class TNStorageManagement (TNArchipelPlugin):
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_DELETE)
         return reply
 
-
     def iq_get(self, iq):
         """
         Get the virtual hatd drives of the virtual machine
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
         try:
             disks = commands.getoutput("ls " + self.entity.folder).split()
             nodes = []
-
             for disk in disks:
                 file_cmd_output = commands.getoutput("file " + self.entity.folder + "/" + disk).lower()
 
@@ -307,15 +279,14 @@ class TNStorageManagement (TNArchipelPlugin):
                 or file_cmd_output.find("data") > -1\
                 or file_cmd_output.find("user-mode linux cow file") > -1) \
                 and file_cmd_output.find("sqlite") == -1:
-                    diskinfo = commands.getoutput("qemu-img info " + self.entity.folder + "/" + disk).split("\n")
-                    node = xmpp.Node(tag="disk", attrs={ "name": disk.split('.')[0],
+                    diskinfo = commands.getoutput(self.qemu_img_bin + " info " + self.entity.folder + "/" + disk).split("\n") ## TODO: use subprocess
+                    node = xmpp.Node(tag="disk", attrs={"name": disk.split('.')[0],
                         "path": self.entity.folder + "/" + disk,
                         "format": diskinfo[1].split(": ")[1],
                         "virtualSize": diskinfo[2].split(": ")[1],
                         "diskSize": diskinfo[3].split(": ")[1],
                     })
                     nodes.append(node)
-
             reply = iq.buildReply("result")
             reply.setQueryPayload(nodes)
             self.entity.log.info("info about disks sent")
@@ -323,39 +294,29 @@ class TNStorageManagement (TNArchipelPlugin):
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_GET)
         return reply
 
-
     def iq_getiso(self, iq):
         """
         Get the virtual cdrom ISO of the virtual machine
-
         @type iq: xmpp.Protocol.Iq
         @param iq: the received IQ
-
         @rtype: xmpp.Protocol.Iq
         @return: a ready to send IQ containing the result of the action
         """
         try:
             nodes = []
-
             isos = commands.getoutput("ls " + self.entity.folder).split()
             for iso in isos:
                 if commands.getoutput("file " + self.entity.folder + "/" + iso).lower().find("iso 9660") > -1:
-                    node = xmpp.Node(tag="iso", attrs={"name": iso, "path": self.entity.folder + "/" + iso })
+                    node = xmpp.Node(tag="iso", attrs={"name": iso, "path": self.entity.folder + "/" + iso})
                     nodes.append(node)
-
             sharedisos = commands.getoutput("ls " + self.shared_isos_folder).split()
             for iso in sharedisos:
                 if commands.getoutput("file " + self.shared_isos_folder + "/" + iso).lower().find("iso 9660") > -1:
-                    node = xmpp.Node(tag="iso", attrs={"name": iso, "path": self.shared_isos_folder + "/" + iso })
+                    node = xmpp.Node(tag="iso", attrs={"name": iso, "path": self.shared_isos_folder + "/" + iso})
                     nodes.append(node)
-
             reply = iq.buildReply("result")
             reply.setQueryPayload(nodes)
             self.entity.log.info("info about iso sent")
         except Exception as ex:
             reply = build_error_iq(self, ex, iq, ARCHIPEL_ERROR_CODE_DRIVES_GETISO)
         return reply
-
-
-
-
