22a3dd076c79f5aefabe9f8bf37433dc3ad01479
Antoine Mercadal <antoine.mercadal@inframonde.eu>
2011-09-16 17:27:50 +0200

    add archipel-commandsbytag in install scripts

--- ArchipelAgent/archipel-agent/setup.py
+++ ArchipelAgent/archipel-agent/setup.py
@@ -91,6 +91,7 @@ setup(name='archipel-agent',
         'install/bin/archipel-updatedomain',
         'install/bin/archipel-initinstall',
         'install/bin/archipel-testxmppserver',
+        'install/bin/archipel-commandsbytag',
         'install/bin/runarchipel'
         ],
       data_files=[
