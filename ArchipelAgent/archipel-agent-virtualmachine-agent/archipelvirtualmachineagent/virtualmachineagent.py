0c0d6da85646ac874eb1869f444ed8f19d75fd92
behrooz shabani (everplays) <behrooz@rock.com>
2012-06-25 15:30:56 +0430

    implemented sending / recieving iq message to/from guest agent
    btw it doesn't work as expected now, as it sends messages with invalid
    from attribute (pubsub.PARAM_XMPP_SERVER) it should use jid of vm for
    sending messages

--- ArchipelAgent/archipel-agent-virtualmachine-agent/archipelvirtualmachineagent/virtualmachineagent.py
+++ ArchipelAgent/archipel-agent-virtualmachine-agent/archipelvirtualmachineagent/virtualmachineagent.py
@@ -19,7 +19,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import xmpp
 from archipelcore.archipelPlugin import TNArchipelPlugin
+
+# Namespace
+ARCHIPEL_NS_GUEST_CONTROL                  = "archipel:guest:control"
 
 class TNVirtualMachineAgent(TNArchipelPlugin):
     """
@@ -36,13 +40,21 @@ class TNVirtualMachineAgent(TNArchipelPlugin):
         @param entry_point_group: the group name of plugin entry_point
         """
         TNArchipelPlugin.__init__(self, configuration=configuration, entity=entity, entry_point_group=entry_point_group)
-        register_items = [{
-			"commands" : ["!exec"],
-			"parameters": [],
-			"method": self.msg_get,
-			"description": "runs command on guest os and displays result"
-		}]
-        self.entity.add_message_registrar_items(register_items)
+        registrar_items = [{
+            "commands" : ["!exec"],
+            "parameters": [],
+            "method": self.exec_message,
+            "description": "runs command on guest os and displays result"
+        }]
+        self.entity.add_message_registrar_items(registrar_items)
+
+    def register_handlers(self):
+        TNArchipelPlugin.register_handlers(self)
+        self.entity.xmppclient.RegisterHandler('iq', self.process_iq, ns=ARCHIPEL_NS_GUEST_CONTROL)
+
+    def unregister_handlers(self):
+        TNArchipelEntity.unregister_handlers(self)
+        self.entity.xmppclient.UnregisterHandler('iq', self.process_iq, ns=ARCHIPEL_NS_GUEST_CONTROL)
 
     @staticmethod
     def plugin_info():
@@ -56,5 +68,30 @@ class TNVirtualMachineAgent(TNArchipelPlugin):
                     "configuration-section"     : None,
                     "configuration-tokens"      : []}
 
-    def msg_get(self, msg):
-        return self.entity.domain.UUIDString()
+    def exec_message(self, msg):
+        """
+        example xml:
+        <iq type="get" to="...">
+            <query ns="...">
+                <archipel action="exec"><!CDATA[[ls]]></archipel>
+            </query>
+        </iq>
+        """
+        request = xmpp.protocol.Iq(typ='get',
+           to=self.entity.domain.UUIDString()+'-agent@'+self.entity.jid.getDomain()+'/guestagent')
+        request.setQueryNS(ARCHIPEL_NS_GUEST_CONTROL);
+        query = request.getTag("query");
+        archipel = query.addChild('archipel');
+        archipel.setAttr('executor', msg.getFrom())
+        archipel.setAttr('action', 'exec');
+        archipel.addData(msg.getBody().replace('!exec', ''))
+        self.entity.log.info('sending: '+str(request))
+        return self.entity.xmppclient.send(request)
+
+    def process_iq(self, con, iq):
+        if iq.getFrom()==self.entity.domain.UUIDString()+'-agent@'+self.entity.jid.getDomain()+'/guestagent':
+            archipel = iq.getTag("query").getTag("archipel")
+            msg = xmpp.protocol.Message(archipel.getAttr('executor'), archipel.getCData())
+            con.send(msg)
+            raise xmpp.protocol.NodeProcessed
+
