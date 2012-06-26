d5b7df9c25764c74c357de119688ad62d1f6c1ef
behrooz shabani (everplays) <behrooz@rock.com>
2012-06-26 15:04:29 +0430

    use clousers for processing message instead of registraration

--- ArchipelAgent/archipel-agent-virtualmachine-agent/archipelvirtualmachineagent/virtualmachineagent.py
+++ ArchipelAgent/archipel-agent-virtualmachine-agent/archipelvirtualmachineagent/virtualmachineagent.py
@@ -40,20 +40,15 @@ class TNVirtualMachineAgent(TNArchipelPlugin):
         @param entry_point_group: the group name of plugin entry_point
         """
         TNArchipelPlugin.__init__(self, configuration=configuration, entity=entity, entry_point_group=entry_point_group)
-        registrar_items = [{
-            "commands" : ["!exec"],
-            "parameters": [],
-            "method": self.exec_message,
-            "description": "runs command on guest os and displays result"
-        }]
-        self.entity.add_message_registrar_items(registrar_items)
 
     def register_handlers(self):
         TNArchipelPlugin.register_handlers(self)
+        self.entity.xmppclient.RegisterHandler('message', self.process_message, typ="chat")
         self.entity.xmppclient.RegisterHandler('iq', self.process_iq, ns=ARCHIPEL_NS_GUEST_CONTROL)
 
     def unregister_handlers(self):
         TNArchipelEntity.unregister_handlers(self)
+        self.entity.xmppclient.UnregisterHandler('message', self.process_message, typ="chat")
         self.entity.xmppclient.UnregisterHandler('iq', self.process_iq, ns=ARCHIPEL_NS_GUEST_CONTROL)
 
     @staticmethod
@@ -64,34 +59,36 @@ class TNVirtualMachineAgent(TNArchipelPlugin):
         @return: dictionary contaning plugin informations
         """
         return {    "common-name"               : "Virtual Machine Agent",
-                    "identifier"                : "virtualmachineagent",
+                    "identifier"                : "virtualmachineguestagent",
                     "configuration-section"     : None,
                     "configuration-tokens"      : []}
 
-    def exec_message(self, msg):
-        """
-        example xml:
-        <iq type="get" to="...">
-            <query ns="...">
-                <archipel action="exec"><!CDATA[[ls]]></archipel>
-            </query>
-        </iq>
-        """
-        request = xmpp.protocol.Iq(typ='get',
-           to=self.entity.domain.UUIDString()+'-agent@'+self.entity.jid.getDomain()+'/guestagent')
-        request.setQueryNS(ARCHIPEL_NS_GUEST_CONTROL);
-        query = request.getTag("query");
-        archipel = query.addChild('archipel');
-        archipel.setAttr('executor', msg.getFrom())
-        archipel.setAttr('action', 'exec');
-        archipel.addData(msg.getBody().replace('!exec', ''))
-        self.entity.log.info('sending: '+str(request))
-        return self.entity.xmppclient.send(request)
-
-    def process_iq(self, con, iq):
-        if iq.getFrom()==self.entity.domain.UUIDString()+'-agent@'+self.entity.jid.getDomain()+'/guestagent':
+    def process_iq(self, conn, iq):
+        self.entity.log.info(str(iq.getFrom()).lower()+'=='+(self.entity.uuid+'-agent@'+self.entity.jid.getDomain()+'/guestagent').lower())
+        if str(iq.getFrom()).lower()==(self.entity.uuid+'-agent@'+self.entity.jid.getDomain()+'/guestagent').lower():
             archipel = iq.getTag("query").getTag("archipel")
-            msg = xmpp.protocol.Message(archipel.getAttr('executor'), archipel.getCData())
-            con.send(msg)
-            raise xmpp.protocol.NodeProcessed
+            msg = xmpp.protocol.Message(archipel.getAttr('executor'), archipel.getData())
+            conn.send(msg)
+            raise xmpp.NodeProcessed
+
+    def process_message(self, conn, msg):
+        body = str(msg.getBody())
+        if body.find('!exec')==0 and self.entity.permission_center.check_permission(str(msg.getFrom().getStripped()), "message"):
+            command = body.replace('!exec', '').strip()
+            executor = msg.getFrom()
+            runIq = self.execute(command, executor)
+            self.entity.log.info('sending: '+str(runIq ))
+            conn.send(runIq)
+            raise xmpp.NodeProcessed
+
+    def execute(self, command, executor):
+        to = xmpp.JID(self.entity.uuid+'-agent@'+self.entity.jid.getDomain()+'/guestagent')
+        iq = xmpp.protocol.Iq(typ='get', to=to)
+        iq.setQueryNS(ARCHIPEL_NS_GUEST_CONTROL);
+        query = iq.getTag("query");
+        archipel = query.addChild('archipel');
+        archipel.setAttr('executor', executor)
+        archipel.setAttr('action', 'exec')
+        archipel.addData(command)
+        return iq
 
