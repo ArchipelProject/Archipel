08c93dae543173e0b52cc2b8fd3315f25b971031
Antoine Mercadal <antoine.mercadal@gmail.com>
2013-03-12 11:02:09 -0700

    Force ci to remove a folder that causes problem.

--- ci.py
+++ ci.py
@@ -170,6 +170,7 @@ if __name__ == "__main__":
     Simple script that can be run using CruiseControl.rb to make continuous integration
     """
 
+    os.system("rm -rf /home/cruise/.cruise/projects/Archipel/work/ArchipelClient/ModulesSources/ToolbarMigration")
     ret, out = commands.getstatusoutput("git log -n1")
     if "#nobuild" in out:
         os.system("echo \* Build skipped according to last commit message (contains #nobuild)")
