586b28daa61cc6b5de8e82dc5228a28aa6aa0fef
Cyril Peponnet <cyril@peponnet.fr>
2015-04-04 10:43:33 -0700

    Avoid duplicate entry in SRG display groups

--- ArchipelAgent/archipel-agent-xmppserver/archipelagentxmppserver/xmppserver.py
+++ ArchipelAgent/archipel-agent-xmppserver/archipelagentxmppserver/xmppserver.py
@@ -428,9 +428,8 @@ class TNXMPPServerController (TNArchipelPlugin):
         @param display: list fo group(s) which can see the group
         """
         server = self.entity.jid.getDomain()
-        # sanitize the display group list
-        santized_display_group = [group for group in display if group.strip()]
-        display_groups = '\n'.join(map(str, santized_display_group))
+        santized_display_group = list(set([group for group in display if group.strip()]))
+        display_groups = '\\n'.join(map(str, santized_display_group))
         answer = self._send_xmlrpc_call("srg_create", {"host": server, "display": display_groups, "name": name, "description": description, "group": ID})
         if not answer['res'] == 0:
             raise Exception("Cannot create shared roster group. %s" % str(answer))
